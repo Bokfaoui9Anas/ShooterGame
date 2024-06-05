fb99d9bf9ebee854e43444dce6e274af815e3c04
Bokfaoui9Anas <bokfaoui.anas@gmail.com>
2024-06-05 23:59:30 +0100

    commit1

---
+++ Assets/Scripts/Controllers/PlayerContoller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Controllers
+{
+    public class PlayerContoller : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
